adf6e6e6056bb714f3a58c8a54d100ebf6144cdc
azizulhakims <it.azizulhakim@gmsil.com>
2022-08-17 17:26:19 +0600

    add

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,16 @@
 // console.log('hello')
+
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    // console.log('hello')
+
+    const newWithdrawAmount = getInputFieldValueById('withdeaw-field');
+    const previusWithdeawTotal = getTextElementValueById('withdeaw-total');
+
+    const newWithdeawTotal = previusWithdeawTotal + newWithdrawAmount;
+    setTextElementValueId('withdeaw-total', newWithdeawTotal);
+
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    setTextElementValueId('balance-total', newBalanceTotal);
+})
