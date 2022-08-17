334cb2652bcbfb14a707b858a805cf287c632260
azizulhakims <it.azizulhakim@gmsil.com>
2022-08-17 16:21:29 +0600

    add fungtions

--- js/deposit.js
+++ js/deposit.js
@@ -3,8 +3,28 @@ function getInputFieldValueById(inputId){
     const inputField = document.getElementById(inputId);
     const inputFieldValueString = inputField.value;
     const inputFieldValue = parseFloat(inputFieldValueString);
-    
+    inputField.value = '';
     return inputFieldValue;
+}
+
+// function getElementValueById (elementId){
+//     const element = document.getElementById(elementId);
+//     const elementValueString = element.innerText;
+//     const value = parseFloat(elementValueString);
+//     return value;
+// }
+
+function getTextElementValueById(elementId){
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const textElementValue = parseFloat(textElementValueString);
+    return textElementValue;  
+}
+
+
+function setTextElementValueId(elementId, newVelue){
+    const textElement =document.getElementById(elementId);
+    textElement.innerText = newVelue;
 }
 
 
@@ -12,5 +32,18 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     // console.log('deposit button click');
 
     const newDepositAmount = getInputFieldValueById('deposit-field');
+
+    const previousDepositTotal = getTextElementValueById('deposit-total');
+    // console.log(previousDepositTotal);
+
+
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+
+    setTextElementValueId('deposit-total', newDepositTotal);
+
+    const previousBalanceTotal = getTextElementValueById('balance-total')
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+    setTextElementValueId('balance-total', newBalanceTotal);
 
 })
