bf57f2a196360bebf1fc420cc4ef4a06aaee346f
azizulhakims <it.azizulhakim@gmsil.com>
2022-08-17 16:23:13 +0600

    add untilitis

--- js/utilitis.js
+++ js/utilitis.js
@@ -1,1 +1,28 @@
 // console.log('hello')
+function getInputFieldValueById(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+// function getElementValueById (elementId){
+//     const element = document.getElementById(elementId);
+//     const elementValueString = element.innerText;
+//     const value = parseFloat(elementValueString);
+//     return value;
+// }
+
+function getTextElementValueById(elementId){
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const textElementValue = parseFloat(textElementValueString);
+    return textElementValue;  
+}
+
+
+function setTextElementValueId(elementId, newVelue){
+    const textElement =document.getElementById(elementId);
+    textElement.innerText = newVelue;
+}
